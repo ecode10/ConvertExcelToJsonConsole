0a06293136597f99144abe3ce4ca5f27cf48365e
Mauricio Junior <mauricio.junior@gmail.com>
2021-11-22 15:53:46 -0600

    add

---
+++ ConvertExcelToJson/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConvertCsvToJson;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ConvertExcelToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var path = @"C:\Temp\Data.xlsx";
+
+            //var excel = new ExcelToJsonLibrary.ExcelToJson();
+            //var result = excel.GetJson(path, "Sheet1");
+
+            //Console.WriteLine(result);
+
+            var main = new Main();
+            var result = ((IMain)main).ConvertCsvFileToJsonObject("C:/temp/Data.csv");
+
+            dynamic u = JsonConvert.DeserializeObject(result);
+
+            var user = JsonConvert.DeserializeObject<IList<UserModel>>(result);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConvertExcelToJson/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertExcelToJson
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+
+        public string Age { get; set; }
+
+        public string Email { get; set; }
+
+        public string Salary { get; set; }
+
+        //public string Type { get; set; }
+
+    }
+}
